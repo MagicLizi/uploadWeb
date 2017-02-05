270522152c42137894cfa94b5744f275e4b61777
magiclizi <407202761@qq.com>
2017-02-05 17:21:24 +0800

    first commit

---
+++ src/routes/IndexPage.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { connect } from 'dva';
+const FileUpload = require('react-fileupload');
+import IndexCSS from './IndexPage.css';
+class IndexPage extends React.Component{
+  render(){
+    var options={
+      baseUrl:'http://127.0.0.1:3000/upload',
+      chooseAndUpload:true,
+      fileFieldName:'userCre',
+      doUpload : function(files,mill){
+        console.log('you just uploaded',typeof files == 'string' ? files : files[0].name)
+      },
+      uploading : function(progress){
+        console.log('loading...',progress.loaded/progress.total+'%')
+      },
+      uploadSuccess : function(resp){
+        console.log('upload success..!');
+        alert(`上传成功！文件地址为${resp.fileUrl}`)
+      },
+      uploadError : function(err){
+        alert(err.message)
+      },
+      uploadFail : function(resp){
+        alert(resp)
+      }
+    }
+    return (
+      <div className = {IndexCSS['container']}>
+        <FileUpload options={options}>
+          <button className = {IndexCSS['btn']} ref="chooseAndUpload">点击上传文件</button>
+        </FileUpload>
+      </div>
+    );
+  }
+}
+
+IndexPage.propTypes = {
+};
+
+export default connect()(IndexPage);
